bbebd93852c4159807834e99a3da0b3e5d3d80f3
Maksym Petrykin <maxpetrikin@gmail.com>
2026-07-08 14:24:13 +0200

    security: hardstop production without Gmail Fernet key

--- alerts/apps.py
+++ alerts/apps.py
@@ -1,7 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class AlertsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "alerts"
     verbose_name = "Почта и обращения"
+
+    def ready(self):
+        if not settings.DEBUG and not getattr(settings, "GMAIL_OAUTH_TOKEN_FERNET_KEY", "").strip():
+            raise ImproperlyConfigured(
+                "GMAIL_OAUTH_TOKEN_FERNET_KEY must be configured when DJANGO_DEBUG=False."
+            )
