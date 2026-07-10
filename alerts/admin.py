ff8bc9a409660ee02821c2bd1ca357acac3fd181
Maksym Petrykin <maxpetrikin@gmail.com>
2026-07-10 11:58:43 +0200

    feat: enable mailbox fetch period admin controls

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,5 +1,6 @@
 from .admin_site.alerts import MarketplaceAlertAdmin, NoiseAlertAdmin
 from .admin_site.mailboxes import MailboxAccountAdmin
+from .admin_site.mailbox_fetch_period import configure_mailbox_fetch_period_admin
 from .admin_site.system import (
     ArgusSettingsAdmin,
     LeadFlagAdmin,
@@ -9,6 +10,9 @@ from .admin_site.system import (
 )
 from .admin_site.ui import NeedsAttentionFilter, status_badge
 from .telegram.sender import send_telegram_alert
+
+
+configure_mailbox_fetch_period_admin(MailboxAccountAdmin)
 
 
 __all__ = [
